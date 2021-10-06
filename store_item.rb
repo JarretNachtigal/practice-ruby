eaa3d7adadbfa6bed3eb516fe522c0763e3c0ec0
Jarret Nachtigal <jarretn1224@gmail.com>
2021-10-06 14:51:32 -0400

    added inheritance_example file and added Vehicle class

--- store_item.rb
+++ store_item.rb
@@ -1,11 +1,3 @@
-# item1 = { :name => "baseball bat", :price => 100, :color => "purple" }
-# item2 = { name: "baseball glove", price: 120, color: "black" }
-# item3 = { name: "batting gloves", price: 50, color: "white" }
-
-# p item1[:name]
-# p item2[:name]
-# p item3[:color]
-
 class Item
   attr_reader :name, :price, :color
   attr_writer :name, :color
@@ -19,31 +11,6 @@ class Item
   def details
     puts "a #{color} #{name} is worth #{@price} dollars."
   end
-
-  # def name
-  #   @name
-  # end
-
-  # def name=(name)
-  #   @name = name
-  # end
-
-  # def price
-  #   @price
-  # end
-
-  # maybe dont want this one
-  # def price=(price)
-  #   @price = price
-  # end
-
-  # def color
-  #   @color
-  # end
-
-  # def color=(color)
-  #   @color = color
-  # end
 end
 
 item1 = Item.new({ name: "baseball glove", price: 120, color: "black" })
