926791c91a89bcf52b96751164f74269e56814dd
Jarret Nachtigal <jarretn1224@gmail.com>
2021-10-06 16:51:04 -0400

    exercises 3 and 4 added

--- exercises/exercises.rb
+++ exercises/exercises.rb
@@ -26,3 +26,30 @@
 # puts died(hopkins)
 
 # 3 ------------
+
+# puts "Greetings! What is your year of origin?"
+# origin = gets.chomp.to_i
+# if origin < 1900
+#   puts 'That\'s the past!'
+# elsif origin > 1900 && origin < 2020
+#   puts "That's the present!"
+# else
+#   puts "That's the future!"
+# end
+
+# 4 -----------
+
+class Person
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def to_string
+    return "#{@last_name}, #{@first_name}"
+  end
+end
+
+tj = Person.new("Thomas", "Jefferson")
+puts tj.to_string
+puts tj
