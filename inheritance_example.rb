2ca231a4bcfa698a5f03049145da40de8eee2728
Jarret Nachtigal <jarretn1224@gmail.com>
2021-10-06 15:21:02 -0400

    added new variables to Car and Bike classes while staying dry

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,9 +1,9 @@
 class Vehicle
   attr_reader :speed, :direction
 
-  def initialize
-    @speed = 0
-    @direction = "north"
+  def initialize(options = { speed: 0, direction: "north" })
+    @speed = options[:speed].to_i
+    @direction = options[:direction]
   end
 
   def brake
@@ -20,23 +20,36 @@ class Vehicle
 end
 
 class Car < Vehicle
+  def initialize(options)
+    super
+    @fuel = options[:fuel].to_i
+    @make = options[:make]
+    @model = options[:model]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  def initialize(options)
+    super
+    @type = options[:type]
+    @weight = options[:weight].to_i
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-bike = Bike.new()
-car = Car.new()
+bike = Bike.new({ type: "idklmao", weight: 15 })
+car = Car.new({ fuel: 50, make: "subaru", model: "outback" })
 
 bike.accelerate
 car.accelerate
 bike.ring_bell
 car.honk_horn
-puts car.speed
-puts bike.speed
+# puts car.speed
+# puts bike.speed
