64b76e0fd6b51ee5707ffce60f64dfe344b80c97
Jarret Nachtigal <jarretn1224@gmail.com>
2021-10-06 16:35:10 -0400

    exercise 1 and 2

---
+++ exercises/exercises.rb
@@ -0,0 +1,28 @@
+# 1 -----------
+
+# puts "Hello!"
+# greeting = gets.chomp()
+# if greeting == "Arrr!"
+#   puts "Go away, pirate."
+# elsif puts "Greetings, hater of pirates!"
+# end
+
+# 2 -----------
+
+# dickens = { name: "Charles Dickens,", year: "1870" }
+# thackeray = { name: "William Thackeray", year: "1863" }
+# trollope = { name: "Anthony Trollope", year: "1882" }
+# hopkins = { name: "Gerard Manley Hopkins", year: "1889" }
+
+# def died(array)
+#   name = array[:name]
+#   year = array[:name]
+#   puts "#{name} died in #{year}."
+# end
+
+# puts died(dickens)
+# puts died(thackeray)
+# puts died(trollope)
+# puts died(hopkins)
+
+# 3 ------------
